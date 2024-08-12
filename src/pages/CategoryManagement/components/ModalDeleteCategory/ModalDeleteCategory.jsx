a01fb7ddc115361060eb7639a5bf2183de550d28
Lam Vu Hoang <lam.vh@dsc.com.vn>
2024-08-13 00:27:45 +0700

    update delete

--- src/pages/CategoryManagement/components/ModalDeleteCategory/ModalDeleteCategory.jsx
+++ src/pages/CategoryManagement/components/ModalDeleteCategory/ModalDeleteCategory.jsx
@@ -2,10 +2,13 @@
 
 import axios from "axios";
 import { Button, Modal } from "flowbite-react";
+import { useState } from "react";
 import toast from "react-hot-toast";
 
 function ModalDeleteCategory({ openModal, setOpenModal, categoryId }) {
+  const [isDeleting, setIsDeleting] = useState(false);
   const handleDelete = async () => {
+    setIsDeleting(true)
     const response = await axios.delete(
       `${import.meta.env.VITE_SUPABASE_URL}/categories?id=eq.${categoryId}`,
       {
@@ -19,13 +22,15 @@ function ModalDeleteCategory({ openModal, setOpenModal, categoryId }) {
     } else {
         toast.error("Xóa danh mục không thành công")
     }
+    setIsDeleting(false)
+    setOpenModal(false)
   };
   return (
     <Modal show={openModal} onClose={() => setOpenModal(false)}>
       <Modal.Header>Xóa danh mục</Modal.Header>
       <Modal.Body>Bạn có chắc chắn muốn xóa danh mục này không?</Modal.Body>
       <Modal.Footer>
-        <Button color="failure" onClick={handleDelete}>
+        <Button color="failure" onClick={handleDelete} isProcessing={isDeleting}>
           Xóa
         </Button>
         <Button color="gray" onClick={() => setOpenModal(false)}>
